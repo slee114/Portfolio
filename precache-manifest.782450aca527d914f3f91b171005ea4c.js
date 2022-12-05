580c91685c2d2f021be2c40cb9284fbcd16bf01c
slee114 <seanlee11403@gmail.com>
2022-12-04 20:49:18 -0800

    Updates

---
+++ precache-manifest.782450aca527d914f3f91b171005ea4c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "00bb34a1a0f708dbadb7",
+    "url": "/portfolio/static/css/main.aee88f9a.chunk.css"
+  },
+  {
+    "revision": "00bb34a1a0f708dbadb7",
+    "url": "/portfolio/static/js/main.00bb34a1.chunk.js"
+  },
+  {
+    "revision": "c2d6691919adc34216af",
+    "url": "/portfolio/static/js/1.c2d66919.chunk.js"
+  },
+  {
+    "revision": "4841c1b43f636756c9bc",
+    "url": "/portfolio/static/js/2.4841c1b4.chunk.js"
+  },
+  {
+    "revision": "9975b3862049410ecad5",
+    "url": "/portfolio/static/js/runtime~main.9975b386.js"
+  },
+  {
+    "revision": "8559718503f3fe8722d2b7bd41a31a40",
+    "url": "/portfolio/static/media/mypic.85597185.png"
+  },
+  {
+    "revision": "39dcf17d514c7a0a27878a159348d918",
+    "url": "/portfolio/static/media/primary-logo-horizontal-stacked.39dcf17d.png"
+  },
+  {
+    "revision": "74e943d82452f81f6a0bb0b51ac6785d",
+    "url": "/portfolio/static/media/java.74e943d8.png"
+  },
+  {
+    "revision": "0785b26f1f578724752c1e194c4d093f",
+    "url": "/portfolio/static/media/python.0785b26f.png"
+  },
+  {
+    "revision": "72bb0af17c541075f1e9135e349bff9d",
+    "url": "/portfolio/static/media/react.72bb0af1.png"
+  },
+  {
+    "revision": "f6236e3285149b031f7ff66b05943a66",
+    "url": "/portfolio/static/media/htmlandcss.f6236e32.png"
+  },
+  {
+    "revision": "bec6dd8276a3664952a2c6b0f0077805",
+    "url": "/portfolio/static/media/Canvas.bec6dd82.png"
+  },
+  {
+    "revision": "c0f128bd9dc359168d2b74fc0f0fd560",
+    "url": "/portfolio/static/media/ztm.c0f128bd.png"
+  },
+  {
+    "revision": "321247fa1e6f904e5b28fac3cf98c932",
+    "url": "/portfolio/static/media/spotify.321247fa.png"
+  },
+  {
+    "revision": "1cda104da8110420157ec85429c2bb15",
+    "url": "/portfolio/index.html"
+  }
+];
